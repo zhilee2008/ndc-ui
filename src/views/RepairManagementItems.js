eb96ffbe8a535aebaccfe9237428d3894150d292
Zhi Li <bjlizhi@cn.ibm.com>
2017-10-07 23:51:08 +0800

    add managermentitems

--- src/views/RepairManagementItems.js
+++ src/views/RepairManagementItems.js
@@ -6,64 +6,86 @@ import {
     CellsTitle,
     Cell,
     CellBody,
-    CellFooter
+    CellFooter,
+    CellHeader,
+    Label
 } from '../../packages';
 import Page from '../components/page';
 
 class RepairManagementItems extends Component {
-    
-        constructor(props){
-            super(props)
-    
-            this.state = {
-                items: [...Array(10).keys()]
-            }
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            items: ['T12311123', 'T24223423', 'T34212343']
         }
-        detailsItemClick = (pageId) => {
-            let path = '/orderdetails';
-            
-            this.props.history.push(path);
-          };
-    
-        render(){
-            return (
-                <InfiniteLoader
-                    onLoadMore={ (resolve, finish) => {
-                        //mock request
-                        setTimeout( ()=> {
-                            if(this.state.items.length > 5){
-                                finish()
-                            }else{
-                                this.setState({
-                                    items: this.state.items.concat([this.state.items.length])
-                                }, ()=> resolve())
-                            }
-                        }, 1000)
-                    }}
-                >
-                <Page className="infinite" title="订单列表" subTitle="订单列表" >
-    
-                        <CellsTitle>订单列表</CellsTitle>
-                        <Cells>
+    }
+    detailsItemClick = (pageId) => {
+        let path = '/orderdetails';
+
+        this.props.history.push(path);
+    };
+
+    render() {
+        return (
+            <InfiniteLoader
+                onLoadMore={(resolve, finish) => {
+                    //mock request
+                    setTimeout(() => {
+                        if (this.state.items.length > 5) {
+                            finish()
+                        } else {
+                            this.setState({
+                                items: this.state.items.concat([this.state.items.length])
+                            }, () => resolve())
+                        }
+                    }, 1000)
+                }}
+            >
+                <Page className="infinite" >
+                    <Cell className={'titlebar'}>
+                        <CellHeader style={{ height: '65px', marginTop: '25px' }} >
+                            <img style={{ float: 'left' }} src='/images/jiantu@2x.png' />
+                            <div className={'titlebarback'}>
+                                返回
+                     </div>
+                        </CellHeader>
+
+                        <CellBody style={{ textAlign: 'center' }} className={'titlebarcontent'}>
+                            报修状态查询
+                  </CellBody>
+                        <CellFooter style={{ width: '20%' }} >
+                            <img src='/images/menu12@2x.png' />
+                        </CellFooter>
+                    </Cell>
+                    {/* <img src='/images/touying@2x.png' /> */}
+                    <div className={'touying'}>
+                        <div style={{ color: '#1887fc', textAlign: 'center' }} >以下为待接的报修单,请点击了解详情并操作</div>
+                    </div>
+                    <Cells style={{ marginTop: '0px' }}>
                         {
-                            this.state.items.map( (item, i) => {
+                            this.state.items.map((item, i) => {
                                 return (
                                     <Cell onClick={this.detailsItemClick.bind(this)} href="javascript:;" key={i} access>
-                                        <CellBody>
-                                            {'订单编号:'}{item}
+                                        <CellHeader>
+                                            <Label>订单编号</Label>
+                                        </CellHeader>
+                                        <CellBody style={{ marginLeft: '20px', color: 'lightgray' }}>
+                                            {item}
                                         </CellBody>
-                                        <CellFooter/>
                                     </Cell>
                                 )
                             })
                         }
-                        </Cells>
-    
+                    </Cells>
+
                 </Page>
-                </InfiniteLoader>
-            )
-        }
+            </InfiniteLoader>
+        )
     }
+}
 
 export default withRouter(RepairManagementItems);
+
 
