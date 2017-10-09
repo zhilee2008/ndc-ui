cfe111c1c9fb5d9835bf8dcd6e466319ab84442b
bjdqian <bjdqian@cn.ibm.com>
2017-10-09 12:52:30 +0800

    完成时间辅助函数

--- src/utils/Util.js
+++ src/utils/Util.js
@@ -1,20 +1,8 @@
+import moment from 'moment';
 
 function timeStamp2String(time) {
-    var datetime = new Date();
-    datetime.setTime(time);
-    var year = datetime.getFullYear();
-    var month = datetime.getMonth() + 1;
-    var date = datetime.getDate();
-    var hour = datetime.getHours();
-    var minute = datetime.getMinutes();
-    var second = datetime.getSeconds();
-    var mseconds = datetime.getMilliseconds();
-    return year + "-"
-        + month.length < 2 ? ('0' + month) : month + "-"
-            + date.length < 2 ? ('0' + date) : date + " "
-                + hour.length < 2 ? ('0' + hour) : hour + ":"
-                    + minute.length < 2 ? ('0' + minute) : minute + ":"
-                        + second.length < 2 ? ('0' + second) : second;
+
+  return moment.unix(time).format("YYYY-MM-DD hh:mm:ss");
 };
 export default {
     timeStamp2String,
