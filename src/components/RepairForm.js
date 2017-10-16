1c3eff91dc58a11a0db9dff2bbad6ccce96743d1
bjdqian <bjdqian@cn.ibm.com>
2017-10-16 21:21:52 +0800

    部署脚本

--- src/components/RepairForm.js
+++ src/components/RepairForm.js
@@ -382,9 +382,11 @@ class RepairForm extends Component {
 
     componentDidMount() {
 
+
+
         const url = 'http://xn.geekx.cn';
         const jsApiObject = sign('HoagFKDcsGMVCIY2vOjf9gX73yWPTGVXRHKIZHi4E1IoWHbeJ8zz_843FzDl3CfG92Iepakr5Qc_V39F5owV_g', url);
-        alert(location.href);
+        alert(window.location.href);
         wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: 'wx457ecf3c803c3774', // 必填，公众号的唯一标识
