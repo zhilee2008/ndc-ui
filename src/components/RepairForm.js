3410a79b6152cc84744c906660b670f4a59f66b1
Zhi Li <bjlizhi@cn.ibm.com>
2017-10-21 19:06:09 +0800

    android

--- src/components/RepairForm.js
+++ src/components/RepairForm.js
@@ -492,24 +492,24 @@ class RepairForm extends Component {
             });
 
             $('#addimagebutton').on('click', function (event) {
-                alert('image');
+                // alert('image');
                 wx.chooseImage({
                     count: 1,
                     sourceType: ['album', 'camera'],
                     success: function (res) {
-                        alert('imageuploadsuccessful');
+                        // alert('imageuploadsuccessful');
                         self.setState({
                             imageId: res.localIds[0],
                         })
-                        alert('arr' + res);
-                        alert('arr' + res.localData);
-                        wx.getLocalImgData({
-                            localId: res.localIds[0], // 图片的localID
-                            success: function (res) {
-                                var localData = res.localData; // localData是图片的base64数据，可以用img标签显示
-                                self.addImageDev(localData);
-                            }
-                        });
+                        self.addImageDev(res.localIds[0]);
+                        // alert('arr' + res);
+                        // wx.getLocalImgData({
+                        //     localId: res.localIds[0], // 图片的localID
+                        //     success: function (res) {
+                        //         var localData = res.localData; // localData是图片的base64数据，可以用img标签显示
+                        //         self.addImageDev(localData);
+                        //     }
+                        // });
                         
                     }
                 });
