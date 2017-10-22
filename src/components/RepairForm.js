78ee364e3d4d8e210c7b35aff732dfcc8103e0be
Zhi Li <bjlizhi@cn.ibm.com>
2017-10-22 17:55:06 +0800

    git push orign m

--- src/components/RepairForm.js
+++ src/components/RepairForm.js
@@ -442,7 +442,7 @@ class RepairForm extends Component {
                 const url = 'http://xn.geekx.cn/repairsubmit';
                 const jsApiObject = sign(jsapiticket, url);
                 wx.config({
-                    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                     appId: appId, // 必填，公众号的唯一标识
                     jsApiList: ['startRecord',
                         'stopRecord',
