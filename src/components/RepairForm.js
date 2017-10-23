e8978d98220ccb414e1160685ef4edccd861cbf7
Zhi Li <bjlizhi@cn.ibm.com>
2017-10-23 19:58:26 +0800

    jgp

--- src/components/RepairForm.js
+++ src/components/RepairForm.js
@@ -575,11 +575,21 @@ class RepairForm extends Component {
                                     wx.getLocalImgData({
                                         localId: id, // 图片的localID
                                         success: function (res) {
-                                            var localData = res.localData; // localData是图片的base64数据，可以用img标签显示
+
+                                            var localData = res.localData.replace('jgp', 'jpeg'); // localData是图片的base64数据，可以用img标签显示
                                             self.addImageDev(localData);
                                             self.state.imageUrlArr.push(localData);
+
+
                                         }
                                     });
+
+
+                                    
+
+
+
+
                                 }
                             }
                         }
@@ -937,6 +947,7 @@ class RepairForm extends Component {
         $('.deleteimage').click(function (e) {
             e.target.parentNode.remove();
             self.setState({
+                imagecount:self.state.imagecount - 1,
                 imageUrlArr: self.state.imageUrlArr.remove(e.target.src)
             });
         });
