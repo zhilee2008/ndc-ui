c5bf81f03f0a97300b104d1b974abd116c470c61
Zhi Li <bjlizhi@cn.ibm.com>
2017-10-23 21:51:02 +0800

    upload more than 1 pic

--- src/components/RepairForm.js
+++ src/components/RepairForm.js
@@ -572,23 +572,7 @@ class RepairForm extends Component {
                                 self.setState({
                                     imageIdArr: res.localIds,
                                 })
-                                // for (var i = 0; i < res.localIds.length; i++) {
-                                // alert("id:"+res.localIds[i]);
-                                self.shownImage(res.localIds[0], res.localIds, 0);
-                                // wx.getLocalImgData({
-                                //     localId: res.localIds[i], // 图片的localID
-                                //     success: function (res) {
-                                //         alert("data" + JSON.stringify(res));
-                                //         var localData = res.localData.replace('jgp', 'jpeg'); // localData是图片的base64数据，可以用img标签显示
-                                //         self.addImageDev(localData);
-                                //         self.state.imageUrlArr.push(localData);
-                                //         shownImage(res[i+1],res);
-                                //     },
-                                //     fail: function (res) {
-                                //         alert(JSON.stringify(res));
-                                //     }
-                                // });
-                                // }
+                                self.showImage(res.localIds[0], res.localIds, 0);
                             }
                         }
 
@@ -603,17 +587,16 @@ class RepairForm extends Component {
 
     }
 
-    shownImage(id, localids, i) {
+    showImage(id, localids, i) {
         const self = this;
-
         wx.getLocalImgData({
             localId: id, // 图片的localID
             success: function (res) {
-                alert("data" + JSON.stringify(res));
+                // alert("data" + JSON.stringify(res));
                 var localData = res.localData.replace('jgp', 'jpeg'); // localData是图片的base64数据，可以用img标签显示
                 self.addImageDev(localData);
                 self.state.imageUrlArr.push(localData);
-                alert(i+1);
+                // alert(i+1);
                 if (i + 1 < localids.length) {
                     self.shownImage(localids[i + 1], localids, i + 1);
                 }
